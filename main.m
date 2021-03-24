2b69517ff7ef820ef41450b463188201f95d3a88
kslam07 <trykslam@gmail.com>
2021-03-24 18:49:14 +0100

    Add plots for operational point plots

--- main.m
+++ main.m
@@ -116,49 +116,19 @@ solver = solver.solveStreamtube();
 % set(gcf,'color','w')
 % export_fig 'plot_enthalpy.png'
 
-% plot CL as function of chord distribution
-
-% compute chord length at each rR
-chordTSR = solver8.computeChordLength(solver6.rR);
-polarData = table2array(readtable("polar_DU95W180.xlsx"));
-LDpoint = polarData(polarData(:,2) == max(polarData(:,2)), :);
-
-figure("defaultAxesFontSize", 18)
-tiledlayout(1,2, "TileSpacing", "compact",'Padding', 'none')
-ax1=nexttile;
-hold on
-plot(chordTSR, solver6.Cl(:,1), "linewidth", 1.5)
-plot(chordTSR, solver8.Cl(:,1), "linewidth", 1.5)
-plot(chordTSR, solver10.Cl(:,1), "linewidth", 1.5)
-legend("$\lambda=6$", "$\lambda=8$", "$\lambda=10$", "interpreter", "latex")
-set(gca, 'XDir','reverse')
-xlabel("chord length [m]")
-ylabel("$C_{L}$", "interpreter", "latex")
-grid
-ax2=nexttile;
-hold on 
-plot(polarData(:,3), polarData(:,2), "linewidth", 1.5)
-scatter(LDpoint(3), LDpoint(2), 50, "Marker", 'x', "lineWidth", 2.5)
-legend("Airfoil polar", "Max. L/D")
-grid
-xlabel("$C_{D}$ [-]", "interpreter", "latex")
-ylabel("$C_{L}$", "interpreter", "latex")
-set(gcf,'color','w');
-linkaxes([ax1 ax2], "y")
-
 %% Plots for TSR
 
-% solver.TSR=6;
-% solver=solver.init();
-% solver6=solver.solveStreamtube();
-% 
-% solver.TSR=8;
-% solver=solver.init();
-% solver8=solver.solveStreamtube();
-% 
-% solver.TSR=10;
-% solver=solver.init();
-% solver10=solver.solveStreamtube();
+solver.TSR=6;
+solver=solver.init();
+solver6=solver.solveStreamtube();
+
+solver.TSR=8;
+solver=solver.init();
+solver8=solver.solveStreamtube();
+
+solver.TSR=10;
+solver=solver.init();
+solver10=solver.solveStreamtube();
 
 % Total Thrust
 % sum(mean(solver6.Ax,2))
@@ -218,15 +188,45 @@ linkaxes([ax1 ax2], "y")
 % set(gcf,'color','w')
 % export_fig 'TSR_F.png'
 
+% plot CL as function of chord distribution
+
+% compute chord length at each rR
+% chordTSR = solver8.computeChordLength(solver6.rR);
+% polarData = table2array(readtable("polar_DU95W180.xlsx"));
+% LDpoint = polarData(polarData(:,2) == max(polarData(:,2)), :);
+% 
+% figure("defaultAxesFontSize", 18)
+% tiledlayout(1,2, "TileSpacing", "compact",'Padding', 'none')
+% ax1=nexttile;
+% hold on
+% plot(chordTSR, solver6.Cl(:,1), "linewidth", 1.5)
+% plot(chordTSR, solver8.Cl(:,1), "linewidth", 1.5)
+% plot(chordTSR, solver10.Cl(:,1), "linewidth", 1.5)
+% legend("$\lambda=6$", "$\lambda=8$", "$\lambda=10$", "interpreter", "latex")
+% set(gca, 'XDir','reverse')
+% xlabel("chord length [m]")
+% ylabel("$C_{L}$", "interpreter", "latex")
+% grid
+% ax2=nexttile;
+% hold on 
+% plot(polarData(:,3), polarData(:,2), "linewidth", 1.5)
+% scatter(LDpoint(3), LDpoint(2), 50, "Marker", 'x', "lineWidth", 2.5)
+% legend("Airfoil polar", "Max. L/D")
+% grid
+% xlabel("$C_{D}$ [-]", "interpreter", "latex")
+% ylabel("$C_{L}$", "interpreter", "latex")
+% set(gcf,'color','w');
+% linkaxes([ax1 ax2], "y")
+
 %% Plots for Skew
 
 solver = BEMSolver;
 solver.nBlades = 3;
 solver.TSR = 8;
-solver.nAnnulus = 100;
+solver.nAnnulus = 500;
 solver.spacing = "0";
 solver.atol = 1e-4;
-solver.nIter = 100;
+solver.nIter = 1000;
 solver.bladePitch = -2;
 solver.nPsi = 100;
 solver.yawAngle = 0;
@@ -259,36 +259,30 @@ y = r.*cos(t);
 % figure("defaultAxesFontSize", 18)
 % t1 = tiledlayout(1,3,'TileSpacing','Compact','Padding','Compact');
 % nexttile
-% pplot = pcolor(x, y, rad2deg(solverS0.alpha));
+% pplot = contourf(x, y, rad2deg(solverS0.alpha));
 % h=colorbar;
 % xlabel(h,'\alpha','Rotation',0,'FontSize',18)
 % xlabel('x/R (-)', 'FontSize',17)
 % ylabel('y/R (-)', 'FontSize',17)
 % grid on
-% set(pplot, "edgeColor", "none");
 % colormap('default')
 % title('\gamma = 0', 'FontSize',17)
-% % colormap("summer")
 % axis square
 % nexttile
-% pplot = pcolor(x, y, rad2deg(solverS15.alpha));
+% pplot = contourf(x, y, rad2deg(solverS15.alpha));
 % h=colorbar;
 % ylabel(h,'\alpha','Rotation',0,'FontSize',18)
 % xlabel('x/R (-)', 'FontSize',17)
-% % ylabel('y/R (-)')
 % grid on
-% set(pplot, "edgeColor", "none");
 % colormap('default')
 % axis square
 % title('\gamma = 15', 'FontSize',17)
 % nexttile
-% pplot = pcolor(x, y, rad2deg(solverS30.alpha));
+% pplot = contourf(x, y, rad2deg(solverS30.alpha));
 % h=colorbar;
 % ylabel(h,'\alpha','Rotation',0,'FontSize',18)
 % xlabel('x/R (-)', 'FontSize',17)
-% % ylabel('y/R (-)')
 % grid on
-% set(pplot, "edgeColor", "none");
 % colormap('default')
 % title('\gamma = 30', 'FontSize',16)
 % axis square
